54d40ddce06f0ef618ef23e0fc6c50ced2fe192e
MaKarin <MaKarin@alabuga.ru>
2025-06-30 14:43:55 +0300

    push

--- client/src/shared/index.js
+++ client/src/shared/index.js
@@ -3,4 +3,4 @@ export { apiFetch } from "./utils/apiFetch.js";
 export { useUser } from "./utils/UserContext";
 export { FormBuilder } from "./components/FormBuilder/FormBuilder.jsx";
 export { Modal } from "./components/Modal/Modal.jsx";
-export { Card, CardContainer } from './components/Card.jsx'
+export { Card, CardContainer } from "./components/Card/Card.jsx";
